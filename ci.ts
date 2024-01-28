0f4849198f596d137519399d43d478b18d658f2f
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-01-28 09:10:54 +0000

    fix issue with dagger connect

--- ci.ts
+++ ci.ts
@@ -2,7 +2,7 @@ import {
   codeQuality,
   test,
   build,
-} from "https://pkg.fluentci.io/flutter_pipeline@v0.7.0/mod.ts";
+} from "https://pkg.fluentci.io/flutter_pipeline@v0.7.1/mod.ts";
 
 await codeQuality();
 await test();
--- example/.fluentci/src/dagger/jobs.ts
+++ example/.fluentci/src/dagger/jobs.ts
@@ -1,6 +1,5 @@
 import { Directory, File, Container } from "../../deps.ts";
-import { Client } from "../../sdk/client.gen.ts";
-import { connect } from "../../sdk/connect.ts";
+import { dag } from "../../sdk/client.gen.ts";
 import { getDirectory } from "./lib.ts";
 
 export enum Job {
@@ -29,35 +28,32 @@ export async function codeQuality(
   src: string | Directory | undefined = ".",
   flutterVersion: string | undefined = "3.13.1"
 ): Promise<File | string> {
-  let id = "";
-  await connect(async (client: Client) => {
-    const context = getDirectory(client, src);
-    const FLUTTER_VERSION = Deno.env.get("FLUTTER_VERSION") || flutterVersion;
-    const ctr = client
-      .pipeline(Job.codeQuality)
-      .container()
-      .from(`ghcr.io/cirruslabs/flutter:${FLUTTER_VERSION}`)
-      .withMountedCache("/root/.pub-cache", client.cacheVolume("pub-cache"))
-      .withEnvVariable("PATH", "$PATH:$HOME/.pub-cache/bin", { expand: true })
-      .withExec(["flutter", "pub", "global", "activate", "dart_code_metrics"])
-      .withDirectory("/app", context, {
-        exclude,
-      })
-      .withWorkdir("/app")
-      .withExec([
-        "sh",
-        "-c",
-        "$HOME/.pub-cache/bin/metrics lib -r codeclimate  > gl-code-quality-report.json",
-      ]);
+  const context = await getDirectory(dag, src);
+  const FLUTTER_VERSION = Deno.env.get("FLUTTER_VERSION") || flutterVersion;
+  const ctr = dag
+    .pipeline(Job.codeQuality)
+    .container()
+    .from(`ghcr.io/cirruslabs/flutter:${FLUTTER_VERSION}`)
+    .withMountedCache("/root/.pub-cache", dag.cacheVolume("pub-cache"))
+    .withEnvVariable("PATH", "$PATH:$HOME/.pub-cache/bin", { expand: true })
+    .withExec(["flutter", "pub", "global", "activate", "dart_code_metrics"])
+    .withDirectory("/app", context, {
+      exclude,
+    })
+    .withWorkdir("/app")
+    .withExec([
+      "sh",
+      "-c",
+      "$HOME/.pub-cache/bin/metrics lib -r codeclimate  > gl-code-quality-report.json",
+    ]);
 
-    await ctr
-      .file("/app/gl-code-quality-report.json")
-      .export("./gl-code-quality-report.json");
+  await ctr
+    .file("/app/gl-code-quality-report.json")
+    .export("./gl-code-quality-report.json");
 
-    await ctr.stdout();
+  await ctr.stdout();
 
-    id = await ctr.file("/app/gl-code-quality-report.json").id();
-  });
+  const id = await ctr.file("/app/gl-code-quality-report.json").id();
   return id;
 }
 
@@ -72,35 +68,32 @@ export async function test(
   src: string | Directory | undefined = ".",
   flutterVersion: string | undefined = "3.13.1"
 ): Promise<Directory | string> {
-  let id = "";
-  await connect(async (client: Client) => {
-    const context = getDirectory(client, src);
-    const FLUTTER_VERSION = Deno.env.get("FLUTTER_VERSION") || flutterVersion;
-    const ctr = client
-      .pipeline(Job.test)
-      .container()
-      .from(`ghcr.io/cirruslabs/flutter:${FLUTTER_VERSION}`)
-      .withMountedCache("/root/.pub-cache", client.cacheVolume("pub-cache"))
-      .withEnvVariable("PATH", "$PATH:$HOME/.pub-cache/bin", { expand: true })
-      .withExec(["flutter", "pub", "global", "activate", "junitreport"])
-      .withDirectory("/app", context, {
-        exclude,
-      })
-      .withWorkdir("/app")
-      .withExec([
-        "sh",
-        "-c",
-        "flutter test --machine --coverage | $HOME/.pub-cache/bin/tojunit -o report.xml",
-      ])
-      .withExec(["sh", "-c", "lcov --summary coverage/lcov.info"])
-      .withExec(["sh", "-c", "genhtml coverage/lcov.info --output=coverage"]);
+  const context = await getDirectory(dag, src);
+  const FLUTTER_VERSION = Deno.env.get("FLUTTER_VERSION") || flutterVersion;
+  const ctr = dag
+    .pipeline(Job.test)
+    .container()
+    .from(`ghcr.io/cirruslabs/flutter:${FLUTTER_VERSION}`)
+    .withMountedCache("/root/.pub-cache", dag.cacheVolume("pub-cache"))
+    .withEnvVariable("PATH", "$PATH:$HOME/.pub-cache/bin", { expand: true })
+    .withExec(["flutter", "pub", "global", "activate", "junitreport"])
+    .withDirectory("/app", context, {
+      exclude,
+    })
+    .withWorkdir("/app")
+    .withExec([
+      "sh",
+      "-c",
+      "flutter test --machine --coverage | $HOME/.pub-cache/bin/tojunit -o report.xml",
+    ])
+    .withExec(["sh", "-c", "lcov --summary coverage/lcov.info"])
+    .withExec(["sh", "-c", "genhtml coverage/lcov.info --output=coverage"]);
 
-    await ctr.file("/app/report.xml").export("./report.xml");
-    await ctr.directory("/app/coverage").export("./coverage");
+  await ctr.file("/app/report.xml").export("./report.xml");
+  await ctr.directory("/app/coverage").export("./coverage");
 
-    await ctr.stdout();
-    id = await ctr.directory("/app/coverage").id();
-  });
+  await ctr.stdout();
+  const id = await ctr.directory("/app/coverage").id();
   return id;
 }
 
@@ -119,46 +112,40 @@ export async function build(
   release = true,
   flutterVersion: string | undefined = "3.13.1"
 ): Promise<File | string> {
-  let id = "";
-  await connect(async (client: Client) => {
-    const context = getDirectory(client, src);
-    const FLUTTER_VERSION = Deno.env.get("FLUTTER_VERSION") || flutterVersion;
-    const BUILD_OUTPUT_TYPE =
-      Deno.env.get("BUILD_OUTPUT_TYPE") || buildType || "apk";
-    const args = [];
+  const context = await getDirectory(dag, src);
+  const FLUTTER_VERSION = Deno.env.get("FLUTTER_VERSION") || flutterVersion;
+  const BUILD_OUTPUT_TYPE =
+    Deno.env.get("BUILD_OUTPUT_TYPE") || buildType || "apk";
+  const args = [];
 
-    if (release) {
-      args.push("--release");
-    }
+  if (release) {
+    args.push("--release");
+  }
 
-    const ctr = client
-      .pipeline(Job.build)
-      .container()
-      .from(`ghcr.io/cirruslabs/flutter:${FLUTTER_VERSION}`)
-      .withMountedCache("/root/.pub-cache", client.cacheVolume("pub-cache"))
-      .withMountedCache(
-        "/app/android/.gradle",
-        client.cacheVolume("android-gradle")
-      )
-      .withMountedCache("/app/build", client.cacheVolume("android-build"))
-      .withDirectory("/app", context, {
-        exclude,
-      })
-      .withWorkdir("/app")
-      .withExec(["flutter", "build", BUILD_OUTPUT_TYPE, ...args])
-      .withExec(["cp", "-r", "build/app/outputs", "/outputs"]);
+  const ctr = dag
+    .pipeline(Job.build)
+    .container()
+    .from(`ghcr.io/cirruslabs/flutter:${FLUTTER_VERSION}`)
+    .withMountedCache("/root/.pub-cache", dag.cacheVolume("pub-cache"))
+    .withMountedCache("/app/android/.gradle", dag.cacheVolume("android-gradle"))
+    .withMountedCache("/app/build", dag.cacheVolume("android-build"))
+    .withDirectory("/app", context, {
+      exclude,
+    })
+    .withWorkdir("/app")
+    .withExec(["flutter", "build", BUILD_OUTPUT_TYPE, ...args])
+    .withExec(["cp", "-r", "build/app/outputs", "/outputs"]);
 
-    await ctr.stdout();
-    id = await ctr
-      .file(
-        `/outputs/${BUILD_OUTPUT_TYPE === "apk" ? "apk" : "bundle"}/${
-          release ? "release" : "debug"
-        }/app-${release ? "release" : "debug"}.${
-          BUILD_OUTPUT_TYPE === "apk" ? "apk" : "aab"
-        }`
-      )
-      .id();
-  });
+  await ctr.stdout();
+  const id = await ctr
+    .file(
+      `/outputs/${BUILD_OUTPUT_TYPE === "apk" ? "apk" : "bundle"}/${
+        release ? "release" : "debug"
+      }/app-${release ? "release" : "debug"}.${
+        BUILD_OUTPUT_TYPE === "apk" ? "apk" : "aab"
+      }`
+    )
+    .id();
   return id;
 }
 
@@ -173,28 +160,22 @@ export async function dev(
   src: Directory | string | undefined = ".",
   flutterVersion: string | undefined = "3.13.1"
 ): Promise<Container | string> {
-  let id = "";
-  await connect(async (client: Client) => {
-    const context = getDirectory(client, src);
-    const FLUTTER_VERSION = Deno.env.get("FLUTTER_VERSION") || flutterVersion;
-    const ctr = client
-      .pipeline(Job.build)
-      .container()
-      .from(`ghcr.io/cirruslabs/flutter:${FLUTTER_VERSION}`)
-      .withMountedCache("/root/.pub-cache", client.cacheVolume("pub-cache"))
-      .withMountedCache(
-        "/app/android/.gradle",
-        client.cacheVolume("android-gradle")
-      )
-      .withMountedCache("/app/build", client.cacheVolume("android-build"))
-      .withDirectory("/app", context, {
-        exclude,
-      })
-      .withWorkdir("/app");
+  const context = await getDirectory(dag, src);
+  const FLUTTER_VERSION = Deno.env.get("FLUTTER_VERSION") || flutterVersion;
+  const ctr = dag
+    .pipeline(Job.build)
+    .container()
+    .from(`ghcr.io/cirruslabs/flutter:${FLUTTER_VERSION}`)
+    .withMountedCache("/root/.pub-cache", dag.cacheVolume("pub-cache"))
+    .withMountedCache("/app/android/.gradle", dag.cacheVolume("android-gradle"))
+    .withMountedCache("/app/build", dag.cacheVolume("android-build"))
+    .withDirectory("/app", context, {
+      exclude,
+    })
+    .withWorkdir("/app");
 
-    await ctr.stdout();
-    id = await ctr.id();
-  });
+  await ctr.stdout();
+  const id = await ctr.id();
   return id;
 }
 
